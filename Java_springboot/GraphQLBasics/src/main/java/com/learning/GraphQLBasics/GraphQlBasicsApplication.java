64a066c93b6ff6d5683b75f708d2f0f0659e3df2
Gaurav <gaurav94.ghosh@gmail.com>
2025-05-03 16:15:53 +0530

    Added Java projects folder and basic setup for GraphQLBasics project

---
+++ Java_springboot/GraphQLBasics/src/main/java/com/learning/GraphQLBasics/GraphQlBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.GraphQLBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlBasicsApplication.class, args);
+	}
+
+}
---
+++ Java_springboot/GraphQLBasics/src/test/java/com/learning/GraphQLBasics/GraphQlBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.GraphQLBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
